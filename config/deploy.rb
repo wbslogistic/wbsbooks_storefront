9717a47f2b39f05d9114667f19c36a5355c26af0
klisha <mans1@ngs.ru>
2014-09-07 02:06:39 +0400

    fix capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:klishevich/#{application}.git"
+set :repository, "git@github.com:wbslogistic/#{application}.git"
 set :branch, "master"
 
 set :rvm_ruby_string, '2.1.2'
