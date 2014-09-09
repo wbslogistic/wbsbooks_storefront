f8f268bf774265753bd8d2be14e502acd2cb614f
klisha <mans1@ngs.ru>
2014-09-10 01:24:48 +0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
+    run "ln -nfs #{shared_path}/public/spree #{release_path}/spree"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
