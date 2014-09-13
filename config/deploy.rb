55cd630ce5d7e5c76789354f223486dd373b4ac5
klisha <mans1@ngs.ru>
2014-09-13 23:42:11 +0400

    deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:wbslogistic/#{application}.git"
-set :branch, "breadcrumbs2"
+set :branch, "master"
 
 set :rvm_ruby_string, '2.1.2@wbsbooks_storefront'
 
@@ -47,8 +47,8 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/breadcrumbs2`
-      puts "WARNING: HEAD is not the same as origin/breadcrumbs2"
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
     end
