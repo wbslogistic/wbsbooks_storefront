a39dcc3c7a1647314a066b44202a68015e39e12f
klisha <mans1@ngs.ru>
2014-09-27 00:53:47 +0400

    improve bulk page grid

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,21 +1,23 @@
 Spree::Product.class_eval do
-  def self.search(name,isbn,author,publisher)  
-  	# where("name like ?", "%#{name}%")
-  	# joins("JOIN spree_product_properties on spree_product_properties.product_id=spree_products.id 
-  	# 	JOIN spree_properties on spree_properties.id = spree_product_properties.property_id").
-  	# 	where("spree_properties.name = ? 
-  	# 		and spree_product_properties.value like ?
-  	# 		and spree_products.name like ?","ISBN","%#{isbn}%", "%#{name}%") 
-    joins("JOIN spree_product_properties pp on pp.product_id=spree_products.id 
-    	JOIN spree_properties p on p.id = pp.property_id 
-    	JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
-    	JOIN spree_taxons t on t.id = pt.taxon_id").
+  def self.search(name,isbn,author,publisher)   
+    # joins("JOIN spree_product_properties pp on pp.product_id=spree_products.id 
+    # 	JOIN spree_properties p on p.id = pp.property_id 
+    # 	JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
+    # 	JOIN spree_taxons t on t.id = pt.taxon_id").
+    # where("lower(spree_products.name) like ? 
+    # 	and p.name = ? 
+    # 	and pp.value like ?
+    # 	and t.permalink like ?
+    # 	and lower(t.name) like ?
+    # 	","%#{name.downcase}%","ISBN","%#{isbn}%", "authors/%", "%#{author.downcase}%")
+
+   joins("JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
+      JOIN spree_taxons t on t.id = pt.taxon_id").
     where("lower(spree_products.name) like ? 
-    	and p.name = ? 
-    	and pp.value like ?
-    	and t.permalink like ?
-    	and lower(t.name) like ?
-    	","%#{name.downcase}%","ISBN","%#{isbn}%", "authors/%", "%#{author.downcase}%")
+      and t.permalink like ?
+      and lower(t.name) like ?
+      ","%#{name.downcase}%", "authors/%", "%#{author.downcase}%")
+
 
   end
 
@@ -29,5 +31,13 @@ Spree::Product.class_eval do
 
   def publisher
   	taxons.where("permalink like ?", "publishers/%").first
+  end
+
+  def price
+    variants_including_master.first.cost_price
+  end
+
+  def qty
+    variants_including_master.first.stock_items.first.count_on_hand
   end
 end
