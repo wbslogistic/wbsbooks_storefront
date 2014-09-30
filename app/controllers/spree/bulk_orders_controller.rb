e151cfe59d26efcecfdd58b320b1b7b4fed0c9d0
klisha <mans1@ngs.ru>
2014-10-01 00:48:22 +0400

    bulk order pagination

--- app/controllers/spree/bulk_orders_controller.rb
+++ app/controllers/spree/bulk_orders_controller.rb
@@ -3,9 +3,9 @@ module Spree
   	layout "bulk_order_layout"
      def index
      	if (!params[:title].blank? or !params[:isbn].blank? or !params[:author].blank? or !params[:publisher].blank?)
-     		@products = Product.search(params[:title],params[:isbn],params[:author], params[:publisher], I18n.locale.to_s)
+     		@products = Product.search(params[:title],params[:isbn],params[:author], params[:publisher], I18n.locale.to_s).page(params[:page]).per(10)
       else
-        @products = Product.all
+        @products = Product.all.page(params[:page]).per(10)
         # @products = [Product.first, Product.second]
      	end
 		# @taxon = Spree::Taxon.find_by_name('Specials') if 'Specials'.present?
