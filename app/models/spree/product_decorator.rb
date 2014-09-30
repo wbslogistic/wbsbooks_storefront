3567d9323b3452f0ab0368de9f929a71e3923f88
klisha <mans1@ngs.ru>
2014-09-30 23:49:49 +0400

    fix bulk order isbn search

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -10,50 +10,55 @@ Spree::Product.class_eval do
     # 	and t.permalink like ?
     # 	and lower(t.name) like ?
     # 	","%#{name.downcase}%","ISBN","%#{isbn}%", "authors/%", "%#{author.downcase}%")
-    Rails.logger.info("!!!locale #{locale}")
-    if (locale == "en")
-      if (!author.blank? or publisher.blank?)
-        joins("LEFT JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
-          LEFT JOIN spree_taxons t on t.id = pt.taxon_id").
-        where("lower(spree_products.name) like ? 
-          and t.permalink like ?
-          and lower(t.name) like ?
-          ","%#{name.downcase}%", "authors/%", "%#{author.downcase}%")              
-      else
-        joins("LEFT JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
-          LEFT JOIN spree_taxons t on t.id = pt.taxon_id").
-        where("lower(spree_products.name) like ? 
-          and t.permalink like ?
-          and lower(t.name) like ?
-          ","%#{name.downcase}%", "publishers/%", "%#{publisher.downcase}%") 
-      end
+    # Rails.logger.info("!!!locale #{locale}")
+    if (!isbn.blank?)
+      joins("JOIN spree_variants v on v.product_id = spree_products.id").
+      where("v.is_master = ? and v.sku = ?", true, isbn)
     else
-      if (locale == "ru")
+      if (locale == "en")
         if (!author.blank? or publisher.blank?)
-          joins("LEFT JOIN spree_products_taxons pt on 
-            pt.product_id = spree_products.id 
-            LEFT JOIN spree_taxons t on t.id = pt.taxon_id 
-            INNER JOIN spree_product_translations p_tr
-            on p_tr.spree_product_id = spree_products.id
-            INNER JOIN spree_taxon_translations t_tr
-            on t_tr.spree_taxon_id = t.id").
-          where("p_tr.locale = ? and lower(p_tr.name) like ? 
-            and t.permalink like ? and t_tr.locale = ?
-            and lower(t_tr.name) like ?",
-            "ru", "%#{name.downcase}%", "authors/%", "ru","%#{author.downcase}%")              
+          joins("LEFT JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
+            LEFT JOIN spree_taxons t on t.id = pt.taxon_id").
+          where("lower(spree_products.name) like ? 
+            and t.permalink like ?
+            and lower(t.name) like ?
+            ","%#{name.downcase}%", "authors/%", "%#{author.downcase}%")              
         else
-          joins("LEFT JOIN spree_products_taxons pt on 
-            pt.product_id = spree_products.id 
-            LEFT JOIN spree_taxons t on t.id = pt.taxon_id 
-            INNER JOIN spree_product_translations p_tr
-            on p_tr.spree_product_id = spree_products.id
-            INNER JOIN spree_taxon_translations t_tr
-            on t_tr.spree_taxon_id = t.id").
-          where("p_tr.locale = ? and lower(p_tr.name) like ? 
-            and t.permalink like ? and t_tr.locale = ?
-            and lower(t_tr.name) like ?",
-            "ru", "%#{name.downcase}%", "publishers/%", "ru","%#{publisher.downcase}%")  
-        end      
+          joins("LEFT JOIN spree_products_taxons pt on pt.product_id = spree_products.id 
+            LEFT JOIN spree_taxons t on t.id = pt.taxon_id").
+          where("lower(spree_products.name) like ? 
+            and t.permalink like ?
+            and lower(t.name) like ?
+            ","%#{name.downcase}%", "publishers/%", "%#{publisher.downcase}%") 
+        end
+      else
+        if (locale == "ru")
+          if (!author.blank? or publisher.blank?)
+            joins("LEFT JOIN spree_products_taxons pt on 
+              pt.product_id = spree_products.id 
+              LEFT JOIN spree_taxons t on t.id = pt.taxon_id 
+              INNER JOIN spree_product_translations p_tr
+              on p_tr.spree_product_id = spree_products.id
+              INNER JOIN spree_taxon_translations t_tr
+              on t_tr.spree_taxon_id = t.id").
+            where("p_tr.locale = ? and lower(p_tr.name) like ? 
+              and t.permalink like ? and t_tr.locale = ?
+              and lower(t_tr.name) like ?",
+              "ru", "%#{name.downcase}%", "authors/%", "ru","%#{author.downcase}%")              
+          else
+            joins("LEFT JOIN spree_products_taxons pt on 
+              pt.product_id = spree_products.id 
+              LEFT JOIN spree_taxons t on t.id = pt.taxon_id 
+              INNER JOIN spree_product_translations p_tr
+              on p_tr.spree_product_id = spree_products.id
+              INNER JOIN spree_taxon_translations t_tr
+              on t_tr.spree_taxon_id = t.id").
+            where("p_tr.locale = ? and lower(p_tr.name) like ? 
+              and t.permalink like ? and t_tr.locale = ?
+              and lower(t_tr.name) like ?",
+              "ru", "%#{name.downcase}%", "publishers/%", "ru","%#{publisher.downcase}%")  
+          end      
+        end
       end
     end
   end
