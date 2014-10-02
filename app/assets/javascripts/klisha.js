2880e350af35e110304b72980d07eda22e852ea8
klisha <mans1@ngs.ru>
2014-10-03 00:39:39 +0400

    select quantaty field on focus bulk_order

--- app/assets/javascripts/klisha.js
+++ app/assets/javascripts/klisha.js
@@ -2,5 +2,6 @@ function bulk_order_sum(element){
 	var str1 = "t"
 	var res = str1.concat(element.id);
 	var price = element.getAttribute("price");
-	document.getElementById(res).innerHTML = element.value * price;
+	var total = element.value * price;
+	document.getElementById(res).innerHTML = total.toFixed(2);
 }
