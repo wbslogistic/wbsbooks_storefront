e9001d7e5372d4be8c1456ff81370b59200dc4ef
krypt2005 <krypt2005@gmail.com>
2014-11-14 14:25:49 +0530

    wishlist changes

---
+++ app/controllers/spree/wished_products_controller.rb
@@ -0,0 +1,52 @@
+class Spree::WishedProductsController < Spree::StoreController
+  respond_to :html
+
+  def create
+    @wished_product = Spree::WishedProduct.new(wished_product_attributes)
+    @wishlist = spree_current_user.wishlist
+
+    if @wishlist.include? params[:wished_product][:variant_id]
+      @wished_product = @wishlist.wished_products.detect {|wp| wp.variant_id == params[:wished_product][:variant_id].to_i }
+    else
+      @wished_product.wishlist = spree_current_user.wishlist
+      @wished_product.save
+    end
+
+    respond_with(@wished_product) do |format|
+      format.html { redirect_to wishlist_url(@wishlist) }
+    end
+  end
+
+  def update
+    @wished_product = Spree::WishedProduct.find(params[:id])
+    @wished_product.update_attributes(wished_product_attributes)
+
+    respond_with(@wished_product) do |format|
+      format.html { redirect_to wishlist_url(@wished_product.wishlist) }
+    end
+  end
+
+  def destroy
+    @wished_product = Spree::WishedProduct.find(params[:id])
+    @wished_product.destroy
+
+    respond_with(@wished_product) do |format|
+      format.html { redirect_to wishlist_url(@wished_product.wishlist) }
+    end
+  end
+
+  def destroy_all
+    @wished_products = Spree::WishedProduct.all
+    puts @wished_products
+    @wished_products.destroy_all
+
+    redirect_to :back
+  end
+
+  private
+
+  def wished_product_attributes
+    params.require(:wished_product).permit(:variant_id, :wishlist_id, :remark)
+  end
+
+end
