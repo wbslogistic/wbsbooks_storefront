27b4ce1e973cd91846b18ed0549a25ad9b440baf
Bharat <bh11.kumar@gmail.com>
2014-12-01 18:57:08 +0530

    add admin user

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,10 +1,12 @@
 user1 = Spree::User.create! :email => 'oleg@evecco.com', :password => '123456', :password_confirmation => '123456'
 user2 = Spree::User.create! :email => 'dmitry@evecco.com', :password => '123456', :password_confirmation => '123456'
 user3 = Spree::User.create! :email => 'katya@evecco.com', :password => '123456', :password_confirmation => '123456'
-
+admin_user = Spree::User.create! :email => 'oakrasnov@rambler.ru', :password => 'WBSpass123', :password_confirmation => 'WBSpass123'
 user1.save
 user2.save
 user3.save
+admin_user.save
+admin_user.spree_roles << Spree::Role.find_by_name(:admin)# provide admin access
 
 puts 'SAMPLE DATA -> New users created'
 
