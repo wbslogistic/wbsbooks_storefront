3adaa062270bf7d16b6d450f587a6fd7e235139d
Serghei <agilestep@yahoo.com>
2014-12-15 11:57:03 +0200

    Adding precompiles
    *to make same link system to both development and production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.assets.precompile += ['owl.carousel.css','owl.theme.css','owlconfig.js','selectize.wbsbooks.css','*.js', '*.css', '*.css.erb']
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
