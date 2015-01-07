0ba2b651162018c32ebfaa1c4ed8f425f619aa46
Serghei <agilestep@yahoo.com>
2015-01-07 20:37:21 +0200

    Adding preferences to rrt calculation

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -7,21 +7,26 @@ def reload_price
     end
 end
 
+
 def rrp publisher_name
   factor = 1.8
+  dynamic_factor = 0.85
+
   case publisher_name
-    when "Эксмо".force_encoding("UTF-8"),"Exmo"
-      factor  *= 0.7
+    when "Эксмо".force_encoding("UTF-8"),"Eksmo",'eksmo'
+      dynamic_factor = Spree::Config.eksmo_rrt ? Spree::Config.eksmo_rrt : 0.7
     when "Azbuka","Азбука".force_encoding("UTF-8")
-      factor  *= 0.72
+      dynamic_factor = Spree::Config.azbuka_rrt ? Spree::Config.azbuka_rrt : 0.72
     when "SZKO","szko","Szko","СЗКО".force_encoding("UTF-8")
-      factor  *= 0.76
-    when "Piter","питер".force_encoding("UTF-8")
-      factor  *= 0.85
+      dynamic_factor = Spree::Config.szko_rrt ? Spree::Config.szko_rrt : 0.76
+    when "Piter","питер".force_encoding("UTF-8"),"Питер".force_encoding("UTF-8")
+      dynamic_factor = Spree::Config.piter_rrt ? Spree::Config.piter_rrt : 0.85
     else
-      factor  *= 0.85
+      dynamic_factor = Spree::Config.default_rrt ? Spree::Config.default_rrt : 0.85
   end
-  rrp_price =  price * factor
+
+  rrp_price =  price * factor * dynamic_factor
+
   Spree::Money.new(rrp_price, { currency: currency })
 end
 
